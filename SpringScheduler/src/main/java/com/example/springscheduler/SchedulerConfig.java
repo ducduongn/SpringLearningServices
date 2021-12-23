0fe3eecf3fdaba5878e5c0b386a7f17de66e8953
ducduongn <48018619+ducduongn@users.noreply.github.com>
2021-12-23 09:38:51 +0700

    scheduler using cron

--- SpringScheduler/src/main/java/com/example/springscheduler/SchedulerConfig.java
+++ SpringScheduler/src/main/java/com/example/springscheduler/SchedulerConfig.java
@@ -1,17 +1,36 @@
 package com.example.springscheduler;
 
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 /**
  * @author ducduongn
  */
 
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+@EnableAsync
 @Configuration
 @ConditionalOnProperty(name = "scheduler.enable", matchIfMissing = true)
 public class SchedulerConfig {
 
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime()
+                        .build()
+        );
+    }
 }
--- SpringScheduler/src/main/java/com/example/springscheduler/jobs/Schedulers.java
+++ SpringScheduler/src/main/java/com/example/springscheduler/jobs/Schedulers.java
@@ -1,8 +1,11 @@
 package com.example.springscheduler.jobs;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -15,8 +18,27 @@ import java.time.ZoneOffset;
 @Slf4j
 public class Schedulers {
 
-    @Scheduled(initialDelay = 2000, fixedRate = 3000)
-    public void testFixDelayScheduler() {
-        log.info("The time is: " + LocalDateTime.now().toEpochSecond(ZoneOffset.UTC));
+    @Scheduled(initialDelayString = "${initial-delay}" , fixedRateString = "${interval}")
+    public void testFixRateJob() {
+        log.info("Fix rate: " + LocalDateTime.now());
     }
+
+    @Scheduled(fixedDelayString = "${interval}")
+    public void testFixDelayJob() {
+        log.info("Fix delay: " + LocalDateTime.now());
+    }
+
+    @Scheduled(cron = "${interval-in-cron}")
+    public void testSchedulerCron() {
+        log.info("Fix cron: " + LocalDateTime.now());
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        final ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(5);
+        return taskScheduler;
+    }
+
+
 }
